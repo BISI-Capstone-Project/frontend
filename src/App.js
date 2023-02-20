851399de0fc2f60a575d7d1c38471dec2179f2c1
Vinícius Gonzalez <vinicius.gonzalez@arbache.com>
2023-02-20 11:45:18 -0500

    Setting up bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+import logo from './ac.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
@@ -7,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         BISI Capston Project
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <small>Coming soon</small>
       </header>
     </div>
   );
