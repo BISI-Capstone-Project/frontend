100dc317c4dfcbf54e52027c93ce3971dbf4f152
vinigonz1993 <vinigonzalez1993@gmail.com>
2023-04-12 08:57:31 -0400

    Removing data from JSON

--- src/Main/Resources.js
+++ src/Main/Resources.js
@@ -1,6 +1,5 @@
 import { Col, Row } from "react-bootstrap";
 import styled from "styled-components";
-import Btn from "../styles/Button";
 import TableContainer from "../styles/TableContainer";
 import TbRow from "../styles/TbRow";
 
@@ -12,21 +11,6 @@ const Link = styled.a`
 const BASE_DIR = 'https://bisi-capstone-project-2023.azurewebsites.net/static';
 
 const RESOURCES = [
-    {
-        'text': 'Volume forecast',
-        'link': 'output.xlsx',
-        'type': 'Volume forecast after applying ML',
-    },
-    {
-        'text': 'Merged file after data processing',
-        'link': 'merged_file.xlsx',
-        'type': 'Combined file used on the ML models',
-    },
-    {
-        'text': 'Original resource data',
-        'link': 'Original_Data.csv',
-        'type': 'Volume Data',
-    },
     {
         'text': 'Mississauga - Daily weather report',
         'link': 'weatherstats_mississauga_daily.csv',
